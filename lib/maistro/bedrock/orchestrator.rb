ad4cd8429f2255503915e45ae4f18c6cbcdfa348
Christophe Verbinnen <christophev@knowbe4.com>
2024-07-30 16:43:55 -0400

    fix

--- lib/maistro/bedrock/orchestrator.rb
+++ lib/maistro/bedrock/orchestrator.rb
@@ -2,14 +2,15 @@ module Maistro
   module Bedrock
     class Orchestrator < Maistro::Orchestrator
       def run(prompt)
-        configuration.bedrock_client.converse(
+        agent_name = configuration.bedrock_client.converse(
           model_id: configuration.model,
           system: [{
             text: system_prompt
           }],
           inference_config: configuration.inference_config,
           messages: [{ role: :user, content: [{ text: prompt }] }]
-        )
+        ).output.message.content.first.text
+        agent_by_name[agent_name]
       end
 
       def configuration
